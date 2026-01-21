7590fec4ad5016c242e2d47490015b8c58753df8
emanisaikumar01 <emanisaikumar01@gmail.com>
2026-01-21 22:31:33 +0530

    Implement binary search for square root calculation

---
+++ Binary_Search/Sqrt_x.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 2:
+            return x
+        low, high=1, x//2
+        ans =0
+        while low<= high :
+            mid= (low + high)//2
+
+            if mid*mid ==x:
+                return mid
+            elif mid * mid<x:
+                ans=mid
+                low =mid+1
+            else:
+                high = mid-1
+        return ans
+            
