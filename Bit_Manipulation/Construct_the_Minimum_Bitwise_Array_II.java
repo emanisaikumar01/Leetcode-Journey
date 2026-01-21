871fa2b7ee0b0fe77b977cc2cad3dd3388d31d97
emanisaikumar01 <emanisaikumar01@gmail.com>
2026-01-21 22:33:25 +0530

    Implement minBitwiseArray method in Solution class

---
+++ Bit_Manipulation/Construct_the_Minimum_Bitwise_Array_II.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+       int n=nums.size();
+       int[] ans= new int[n];
+       for(int i=0;i<n;i++){
+        int p=nums.get(i);
+        if(p==2){
+            ans[i]=-1;
+            continue;
+        }
+        int k=0;
+        int temp=p;
+        while((temp & 1 )==1){
+            k++;
+            temp >>=1;
+        }
+        if(k==0){
+            ans[i]=-1;
+            continue;
+        }
+        ans[i]= p-(1 <<(k-1));
+       }
+       return ans;
+    }
+}
